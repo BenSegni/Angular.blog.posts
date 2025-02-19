70e6231279992be6e94d0fb3c4dca67644218541
BenSegni <BBKSegni@gmail.com>
2025-02-19 15:26:01 +0000

    Incuded RouterOutlet & set default route to routes array

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { BlogPostsComponent } from './components/blog-posts/blog-posts.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: BlogPostsComponent,
+  },
+];
